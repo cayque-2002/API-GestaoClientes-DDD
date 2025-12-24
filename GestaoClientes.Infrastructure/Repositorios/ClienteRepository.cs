235482dd1ed3ad4571cc5c5047cc217e5136fea9
cayque-2002 <cayque.142@gmail.com>
2025-12-24 09:14:25 -0300

    Implementação do repositório do cliente em memória

---
+++ GestaoClientes.Infrastructure/Repositorios/ClienteRepository.cs
@@ -0,0 +1,34 @@
+﻿using GestaoClientes.Application.Interfaces;
+using GestaoClientes.Domain.Entidades;
+using GestaoClientes.Domain.ValueObjetcs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoClientes.Infrastructure.Repositorios
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private static readonly List<Cliente> _clientes = new();
+
+        public Task AdicionarAsync(Cliente cliente)
+        {
+            _clientes.Add(cliente);
+            return Task.CompletedTask;
+        }
+
+        public Task<Cliente?> ObterPorIdAsync(Guid id)
+        {
+            var cliente = _clientes.FirstOrDefault(c => c.Id == id);
+            return Task.FromResult(cliente);
+        }
+
+        public Task<bool> ExisteCnpjAsync(Cnpj cnpj)
+        {
+            var existe = _clientes.Any(c => c.Cnpj.Equals(cnpj));
+            return Task.FromResult(existe);
+        }
+    }
+}
